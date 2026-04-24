636ab60d3cc1e2b1c6bf160347b5ed9ca86d431c
Balaji <balaji@eviridis.com>
2026-04-24 11:17:35 +0530

    notes updated

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 sg_id="sg-0edf74a4c129178e7"
 ami_id="ami-0220d79f3f480ecf5"
+zone_id="Z02669081FB4X7KDSL5OS"
+domain_name="sainu.online"
 
 for instance in $@
  do
@@ -13,5 +15,50 @@ for instance in $@
     --query 'Instances[0].InstanceId' \
     --output text )
 
- done
+     if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi
+
+    echo "IP Address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+done
 
